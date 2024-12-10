6f628d7ce1171d18b2f76184572f5d825da61098
aman-gupta21 <amanguptajaipur32@gmail.com>
2024-12-10 16:40:43 +0530

    first

---
+++ app.js
@@ -0,0 +1,93 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8], 
+    [2, 4, 6], 
+    [3, 4, 5], 
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true; // Reset global variable
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false; // Enable boxes
+        box.innerText = "";   // Clear text
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkDraw = () => {
+    let allFilled = true;
+    for (let box of boxes) {
+        if (box.innerText === "") {
+            allFilled = false;
+            break;
+        }
+    }
+
+    if (allFilled) {
+        msg.innerText = "It's a Draw!";
+        msgContainer.classList.remove("hide");
+    }
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            console.log("Winner", pos1Val);
+            showWinner(pos1Val);
+            return;
+        }
+    }
+
+    checkDraw(); // Check for a draw if no winner
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
